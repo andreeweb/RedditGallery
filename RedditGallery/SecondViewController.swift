b00f966aa3b29d0301501c4752ebe94cacd24ebe
Andrea Cerra <andrea.cerra@me.com>
2020-07-10 20:31:00 +0200

    Initial Commit

---
+++ RedditGallery/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RedditGallery
+//
+//  Created by Andrea Cerra on 7/10/20.
+//  Copyright © 2020 Andrea Cerra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
